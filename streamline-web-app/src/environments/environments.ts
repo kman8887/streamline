bd032a16e68279d77e33f4791518973d0d196bf5
Kman <gt8887@hotmail.co.uk>
2025-04-07 13:43:59 +0100

    ahhh

--- streamline-web-app/src/environments/environments.ts
+++ streamline-web-app/src/environments/environments.ts
@@ -1,5 +1,7 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:5000/api',
-  audienceUrl: 'http://localhost:5000',
+  production: true,
+  apiUrl:
+    'https://streamline-api-e7dkh8e6gtcnakft.uksouth-01.azurewebsites.net/api',
+  audienceUrl:
+    'https://streamline-api-e7dkh8e6gtcnakft.uksouth-01.azurewebsites.net',
 };
