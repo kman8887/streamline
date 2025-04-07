c6f808b99ef8f4a45093ced73974dcd0a71dfa2d
Kman <gt8887@hotmail.co.uk>
2025-04-07 12:35:57 +0100

    Update endpoint protection

--- streamline-web-app/src/environments/environment.prod.ts
+++ streamline-web-app/src/environments/environment.prod.ts
@@ -2,4 +2,6 @@ export const environment = {
   production: true,
   apiUrl:
     'https://streamline-api-e7dkh8e6gtcnakft.uksouth-01.azurewebsites.net/api',
+  audienceUrl:
+    'https://streamline-api-e7dkh8e6gtcnakft.uksouth-01.azurewebsites.net',
 };
